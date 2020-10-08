13d1ff36e4ce383d516f664e63a5b8ad5a313db4
CarloPa <54579322+Carlopasquinucci@users.noreply.github.com>
2020-10-08 12:31:27 +0200

    Help update

--- run.sh
+++ run.sh
@@ -6,11 +6,22 @@
 #    noWriteNow: Stops simulation on completion of current time step and does not write out data.
 #    nextWrite: Stops simulation on completion of next scheduled write time, specified by writeControl.
 #    restart: Change the stopAt to endTime
-#    help: write the help
+#    --help: write the help
 
 
 if [[ $# -eq 0 ]] ; then
-    echo 'Assign an argument, at least writeNow or restart'
+    echo 'Assign an argument, at least writeNow or restart. Run --help for more details'
+    exit 1
+fi
+
+if [[  $1 == "--help"  ]] ; then
+    echo ' Run it as sh run.sh argument'
+    echo ' Arguments available:'
+    echo '    writeNow: Stops simulation on completion of current time step and writes data.'
+    echo '    noWriteNow: Stops simulation on completion of current time step and does not write out data.'
+    echo '    nextWrite: Stops simulation on completion of next scheduled write time, specified by writeControl.'
+    echo '    restart: Change the stopAt to endTime'
+    echo '    --help: write the help'
     exit 1
 fi
 
